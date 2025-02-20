ff93c3f087ce69a9333d0ea41e78620711206d00
Lynn <155890291+tuanyuanbao@users.noreply.github.com>
2025-02-20 15:29:41 +1100

    Delete data warehouse directory

--- data warehouse/code_deploy_copy_files.sh
+++
@@ -1,3 +0,0 @@
-rsync -ad /home/airflow/airflow/code_deploy/plugins/ /home/airflow/airflow/plugins/
-rsync -ad /home/airflow/airflow/code_deploy/dags/ /home/airflow/airflow/dags/
-rsync -ad /home/airflow/airflow/code_deploy/apps/ /home/airflow/airflow/apps/
